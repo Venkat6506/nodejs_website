b5008874c582f85c0984ed0129e3d8c07e178864
Venkat6506 <118205580+Venkat6506@users.noreply.github.com>
2023-01-30 14:12:32 +0530

    Create main.sh

---
+++ hooks/installation/main.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -eu
+
+if ! command -v snyk &> /dev/null
+then
+  SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+  if command -v brew &> /dev/null; then
+    bash "${SCRIPT_DIR}"/install-brew.sh
+  elif command -v scoop &> /dev/null; then
+    bash "${SCRIPT_DIR}"/install-scoop.sh
+  elif command -v npm &> /dev/null; then
+    bash "${SCRIPT_DIR}"/install-npm.sh
+  elif command -v yarn &> /dev/null; then
+    bash "${SCRIPT_DIR}"/install-yarn.sh
+  else
+    bash "${SCRIPT_DIR}"/install-standalone.sh
+  fi
+fi
